ecd4db8f3a9548f15ad5beb07795605740c8e940
Al3xiel <gabriel280104casas@gmail.com>
2024-11-24 00:30:41 -0500

    feat(implementation): added examiner command service implementation.

---
+++ src/main/java/org/hin/platform/u202220033/personnel/application/ExaminerCommandServiceImpl.java
@@ -0,0 +1,32 @@
+package org.hin.platform.u202220033.personnel.application;
+
+import org.hin.platform.u202220033.personnel.domain.model.aggregates.Examiner;
+import org.hin.platform.u202220033.personnel.domain.model.commands.CreateExaminerCommand;
+import org.hin.platform.u202220033.personnel.domain.services.ExaminerCommandService;
+import org.hin.platform.u202220033.personnel.infrastructure.persistence.jpa.repositories.ExaminerRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class ExaminerCommandServiceImpl implements ExaminerCommandService {
+
+    private final ExaminerRepository examinerRepository;
+
+    public ExaminerCommandServiceImpl(ExaminerRepository examinerRepository) {
+        this.examinerRepository = examinerRepository;
+    }
+
+    @Override
+    public Optional<Examiner> handle(CreateExaminerCommand command) {
+
+        var nationalProviderIdentifier = UUID.fromString(command.nationalProviderIdentifier());
+        var examinerInDatabase = this.examinerRepository.findByNationalProviderIdentifier(nationalProviderIdentifier);
+        if(examinerInDatabase.isPresent()) throw new IllegalArgumentException("Examiner with the national provider identifier already exists");
+
+        var examiner = new Examiner(command);
+        var createdExaminer = examinerRepository.save(examiner);
+        return Optional.of(createdExaminer);
+    }
+}
--- src/main/java/org/hin/platform/u202220033/personnel/domain/model/aggregates/Examiner.java
+++ src/main/java/org/hin/platform/u202220033/personnel/domain/model/aggregates/Examiner.java
@@ -6,10 +6,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
 import lombok.NonNull;
 import org.hin.platform.u202220033.assessment.domain.model.aggregates.MentalStateExam;
+import org.hin.platform.u202220033.personnel.domain.model.commands.CreateExaminerCommand;
 import org.hin.platform.u202220033.personnel.domain.model.valueobjects.NationalProviderIdentifier;
 import org.hin.platform.u202220033.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
 
 import java.util.List;
+import java.util.UUID;
 
 @Entity
 public class Examiner extends AuditableAbstractAggregateRoot<Examiner> {
@@ -21,4 +23,14 @@ public class Examiner extends AuditableAbstractAggregateRoot<Examiner> {
     private NationalProviderIdentifier nationalProviderIdentifier;
     @OneToMany(mappedBy = "examiner", cascade = CascadeType.ALL, orphanRemoval = true)
     List<MentalStateExam> mentalStateExams;
+
+    public Examiner() {
+    }
+
+    public Examiner(CreateExaminerCommand command) {
+        this.firstName = command.firstName();
+        this.lastName = command.lastName();
+        var commandNationalProviderIdentifier = UUID.fromString(command.nationalProviderIdentifier());
+        this.nationalProviderIdentifier = new NationalProviderIdentifier(commandNationalProviderIdentifier);
+    }
 }
