deb945f5d6f6b6412d7bd7b61108e3adf9dd37a7
Al3xiel <gabriel280104casas@gmail.com>
2024-11-24 00:29:50 -0500

    fix(commands): fixed commands.

--- src/main/java/org/hin/platform/u202220033/assessment/domain/model/commands/CreateMentalStateExamCommand.java
+++ src/main/java/org/hin/platform/u202220033/assessment/domain/model/commands/CreateMentalStateExamCommand.java
@@ -1,4 +1,4 @@
 package org.hin.platform.u202220033.assessment.domain.model.commands;
 
-public record CreateMentalStateExamCommand(String examDate, String examinerNationalProviderIdentifier, String patientNationalProviderIdentifier) {
+public record CreateMentalStateExamCommand(Long patientId, String examinerNationalProviderIdentifier, String examDate, Integer orientationScore, Integer registrationScore, Integer attentionAndCalculationScore, Integer recallScore, Integer languageScore) {
 }
--- src/main/java/org/hin/platform/u202220033/personnel/domain/model/commands/CreateExaminerCommand.java
+++ src/main/java/org/hin/platform/u202220033/personnel/domain/model/commands/CreateExaminerCommand.java
@@ -1,4 +1,4 @@
 package org.hin.platform.u202220033.personnel.domain.model.commands;
 
-public record CreateExaminerCommand(String firstName, String lastName, String nationalProviderIdentifier, Long patientId, Integer orientationScore, Integer registrationScore, Integer attentionAndCalculationScore, Integer recallScore, Integer languageScore) {
+public record CreateExaminerCommand(String firstName, String lastName, String nationalProviderIdentifier) {
 }
