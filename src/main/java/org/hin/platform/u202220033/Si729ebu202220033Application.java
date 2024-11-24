13eda449c823217c2c8a378acf90562e405173a7
Al3xiel <gabriel280104casas@gmail.com>
2024-11-23 19:32:18 -0500

    chore: setup project.

---
+++ src/main/java/org/hin/platform/u202220033/Si729ebu202220033Application.java
@@ -0,0 +1,13 @@
+package org.hin.platform.u202220033;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Si729ebu202220033Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Si729ebu202220033Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/hin/platform/u202220033/Si729ebu202220033ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hin.platform.u202220033;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Si729ebu202220033ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
