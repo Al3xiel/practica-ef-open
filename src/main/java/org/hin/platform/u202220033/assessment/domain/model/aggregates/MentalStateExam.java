a53e26d43a62f5344ae1a1af8900768d03b27c8d
Al3xiel <gabriel280104casas@gmail.com>
2024-11-24 02:33:39 -0500

    fix(entities): fixed attribute type on MentalStateExam entity.

--- src/main/java/org/hin/platform/u202220033/assessment/domain/model/aggregates/MentalStateExam.java
+++ src/main/java/org/hin/platform/u202220033/assessment/domain/model/aggregates/MentalStateExam.java
@@ -4,17 +4,22 @@ import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.Getter;
 import lombok.NonNull;
+import lombok.Setter;
 import org.hin.platform.u202220033.assessment.domain.model.commands.CreateMentalStateExamCommand;
 import org.hin.platform.u202220033.assessment.domain.model.valueobjects.ExaminerNationalProviderIdentifier;
 import org.hin.platform.u202220033.personnel.domain.model.aggregates.Examiner;
 import org.hin.platform.u202220033.personnel.domain.model.commands.CreateExaminerCommand;
 import org.hin.platform.u202220033.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.UUID;
 
 @Entity
+@Getter
 public class MentalStateExam extends AuditableAbstractAggregateRoot<MentalStateExam> {
     @NonNull
     private Long patientId;
@@ -22,7 +27,7 @@ public class MentalStateExam extends AuditableAbstractAggregateRoot<MentalStateE
     @Embedded
     private ExaminerNationalProviderIdentifier examinerNationalProviderIdentifier;
     @NonNull
-    private Date examDate;
+    private LocalDate examDate;
     @NonNull
     private Integer orientationScore;
     @NonNull
@@ -34,14 +39,19 @@ public class MentalStateExam extends AuditableAbstractAggregateRoot<MentalStateE
     @NonNull
     private Integer languageScore;
 
+    @Setter
     @ManyToOne
     @JoinColumn(name = "examiner_id", nullable = false)
     private Examiner examiner;
 
+    public MentalStateExam() {
+    }
+
     public MentalStateExam(CreateMentalStateExamCommand command) {
         this.patientId = command.patientId();
         this.examinerNationalProviderIdentifier = new ExaminerNationalProviderIdentifier(UUID.fromString(command.examinerNationalProviderIdentifier()));
-        this.examDate = new Date(command.examDate());
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        this.examDate = LocalDate.parse(command.examDate(), formatter);
         this.orientationScore = command.orientationScore();
         this.registrationScore = command.registrationScore();
         this.attentionAndCalculationScore = command.attentionAndCalculationScore();
